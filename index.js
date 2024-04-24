b3014d1cf9ce2e8fdb9c5bcfceac1d5ba0d9207c
shiwanikumari0 <121502410+shiwanikumari0@users.noreply.github.com>
2024-04-25 02:04:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,24 +5,7 @@ import cors from "cors";
 
 import bookRoute from "./route/book.route.js"
 import userRoute from "./route/user.route.js";
-const router = createBrowserRouter([
-  {
-   path: "/",
-   element: Home(),
-  },
-  {
-   path:'/course',
-   element: Courses(),
-  },
-   {
-   path:'/about',
-   element: About(),
-  },
-  {
-   path:'/contact',
-   element: Contact(),
-  }
- ]);
+
 const app = express();
 
 app.use(cors());
